71dc68e519ee1ad93663c958cd5a1f1a99cf1bc7
islammd39e <islammdrahat308@gmail.com>
2023-02-26 12:52:16 +0600

    acordian style

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,3 +17,20 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
